82f8ee8d91907beaa84b62360e00270fb334bf15
Nunik95 <nurana.95@mail.ru>
2022-02-24 17:26:03 +0400

    added semaphore

---
+++ semaphore.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+func main() {
+	c:=make(chan, int)
+	d:=make(chan, bool)
+	go func() {
+		for i:=0; i<10; i++{
+			c<-i
+		}
+	d<-true
+	} ()
+	go func() {
+		for i:=0; i<10; i++{
+			c<-i
+		}
+	d<-true
+	} ()
+	go func() {
+		<-d
+		<-d
+		close(c)
+	} ()
+	for x:=range c {
+		fmt.Println(x)
+	}
+}
